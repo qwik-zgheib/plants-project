caabc3ddfc4a1636751754912a92b89b562623f6
qwik-zgheib <zgheibmossad1010@gmail.com>
2024-01-20 11:00:05 -0500

    feat: update About & Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import Navbar from '../components/Navbar';
-
 const Home: React.FC = () => {
-  return (
-    <div>
-      This is a Home <Navbar />
-    </div>
-  );
+  return <div>This is a Home</div>;
 };
 
 export default Home;
