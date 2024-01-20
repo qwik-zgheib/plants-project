973e3f010b699fdbc49e2e6f5c1b726e0216bb12
qwik-zgheib <zgheibmossad1010@gmail.com>
2024-01-20 11:00:36 -0500

    feat: modify rotes configs

--- src/conf/route.tsx
+++ src/conf/route.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Landing from '../pages/Landing';
+import Navbar from '../components/Navbar';
+import About from '../pages/About';
+import NotFound from '../pages/NotFound';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +13,21 @@ const router = createBrowserRouter([
   },
   {
     path: '/home',
-    element: <Home />,
+    element: <Navbar />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ]);
 
