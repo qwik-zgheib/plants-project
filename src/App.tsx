bdd3b1a22d6b6e1877fa1087ad8c072b92ac253f
qwik-zgheib <zgheibmossad1010@gmail.com>
2024-01-22 22:01:58 -0500

    feat: implement I18nextProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import { RouterProvider } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 
 import router from './conf/route';
+import i18n from './i18n/config';
 
 const App: React.FC = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <I18nextProvider i18n={i18n}>
+        <RouterProvider router={router} />
+      </I18nextProvider>
     </>
   );
 };
