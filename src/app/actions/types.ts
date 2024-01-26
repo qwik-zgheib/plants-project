cefd0de02ca9230af052ee969d71be2844f39ed8
qwik-zgheib <zgheibmossad1010@gmail.com>
2024-01-25 22:45:19 -0500

    feat: add language

--- src/app/actions/types.ts
+++ src/app/actions/types.ts
@@ -1,6 +1,7 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const INCREMENT_BY_AMOUNT = 'INCREMENT_BY_AMOUNT';
+export const CHANGE_LNG = 'CHANGE_LNG';
 
 export interface IncrementAction {
   type: typeof INCREMENT;
@@ -15,4 +16,10 @@ export interface IncrementByAmountAction {
   payload: number;
 }
 
+export interface ChangeLngAction {
+  type: typeof CHANGE_LNG;
+  payload: string;
+}
+
 export type CounterActionTypes = IncrementAction | DecrementAction | IncrementByAmountAction;
+export type LngActionTypes = ChangeLngAction;
--- src/app/reducers/reducer.ts
+++ src/app/reducers/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import languageReducer from './languageSlice';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  language: languageReducer,
 });
 
 export default rootReducer;
