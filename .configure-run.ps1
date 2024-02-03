b2905f54254898f4398bb925ae03ba174ce2ec0a
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-02 21:23:55 -0500

    chore: update config .prettierrc and .configure-run.ps1

--- .configure-run.ps1
+++ .configure-run.ps1
@@ -1,18 +1,27 @@
 # Check OS and configure core.autocrlf in Git
 if ($env:OSTYPE -like 'darwin*' -or $env:OSTYPE -like 'linux-gnu') {
   git config --global core.autocrlf input
+  Write-Host "First option"
 }
 else {
   git config --global core.autocrlf true
+  Write-Host "Second option"
 }
 
-# Check if Node.js and npm are installed
-if (Get-Command -Name node -ErrorAction SilentlyContinue -and Get-Command -Name npm -ErrorAction SilentlyContinue) {
-  Write-Host "Node.js and npm are already installed."
+# Check if Node.js is installed
+if (Get-Command -Name node -ErrorAction SilentlyContinue) {
+    Write-Host "Node.js is already installed."
+} else {
+    Write-Host "Error: Node.js is not installed. Please install it."
+    exit 1
 }
-else {
-  Write-Host "Error: Node.js and npm are not installed. Please install them."
-  exit 1
+
+# Check if npm is installed
+if (Get-Command -Name npm -ErrorAction SilentlyContinue) {
+    Write-Host "npm is already installed."
+} else {
+    Write-Host "Error: npm is not installed. Please install it."
+    exit 1
 }
 
 # Install dependencies and run npm commands
